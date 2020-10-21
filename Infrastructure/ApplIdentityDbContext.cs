d2cb6c40ab0e00cbd253e4ad604159d1b7ae0f6c
GGOShinoAsada <mstr.calling@gmail.com>
2020-10-21 13:20:45 +0300

    create context database

---
+++ Infrastructure/ApplIdentityDbContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using ExecuteIdentityFramework.Models;
+
+namespace ExecuteIdentityFramework.Infrastructure
+{
+    public class ApplIdentityDbContext : IdentityDbContext<AppUser>
+        
+    {
+        
+        public ApplIdentityDbContext() : base("IbentityDb") { }
+        static ApplIdentityDbContext()
+        {
+            Database.SetInitializer<ApplIdentityDbContext>(new IdentityDbInit());
+        }
+        public static ApplIdentityDbContext Create()
+        {
+            return new ApplIdentityDbContext();
+        }
+    }
+    public class IdentityDbInit : DropCreateDatabaseIfModelChanges<ApplIdentityDbContext>
+    {
+        protected override void Seed(ApplIdentityDbContext context)
+        {
+            PerfomInicialSetup(context);
+            base.Seed(context);
+        }
+        public void PerfomInicialSetup(ApplIdentityDbContext context)
+        {
+
+        }
+    }
+}
