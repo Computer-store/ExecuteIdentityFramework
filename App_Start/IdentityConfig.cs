056224df915b4e7ebdc8a37dd863fb54ad707b46
GGOShinoAsada <mstr.calling@gmail.com>
2020-10-21 14:28:52 +0300

    creating IdentityConfig class for work with OEIN infrastructure

---
+++ App_Start/IdentityConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Owin;
+using ExecuteIdentityFramework.Infrastructure;
+
+namespace ExecuteIdentityFramework.App_Start
+{
+    public class IdentityConfig
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.CreatePerOwinContext<ApplIdentityDbContext>(ApplIdentityDbContext.Create);
+            app.CreatePerOwinContext<AppUserManager>(AppUserManager.Create);
+            app.UseCookieAuthentication(new Microsoft.Owin.Security.Cookies.CookieAuthenticationOptions {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie, LoginPath = new PathString("/Account/Login"),
+            });
+        }
+    }
+}
