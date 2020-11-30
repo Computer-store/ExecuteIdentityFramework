937497ed8de582a6d965d0c5e374c85656385f1d
GGOShinoAsada <mstr.calling@gmail.com>
2020-11-30 11:55:30 +0300

    fix validation in create metod

--- Models/UseViewModels.cs
+++ Models/UseViewModels.cs
@@ -3,16 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
 
 namespace ExecuteIdentityFramework.Models
 { 
     public class CreateModel
     {
-        [Required]
+        [Required (ErrorMessage ="Please input name attribute")]
         public string Name { get; set; }
-        [Required]
+        [Required (ErrorMessage ="please enter correct email address")]
+        [RegularExpression(".+\\@.+\\..+" ,ErrorMessage ="you input uncorrect email address, please try ageain")]                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
         public String Email { get; set; }
-        [Required]
+        [Required (ErrorMessage ="Please enter password")]
         public string Password { get; set; }
     }
     
